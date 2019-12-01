9487264e74070ff6ded71beb71b8fba93c37af8a
Felipe Melo <36086450+felipefrm@users.noreply.github.com>
2019-12-01 17:44:21 -0300

    not only square matrix

--- multiplicacao_matriz.cu
+++ multiplicacao_matriz.cu
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define N 3 // numero de colunas das matrizes
-#define M 3  // número de threads por bloco
+#define N 3 
+#define M 3  
+#define K 3
 #define T 8 // numero de threads por bloco
 
 // codigo device
@@ -11,33 +12,33 @@ __global__ void multiplica_matriz(int *a, int *b, int *c){
   int j = blockIdx.y * blockDim.y + threadIdx.y;
   c[N*i+j] = 0;
   if (i < M && j < N)
-    for (int k = 0; k<N; k++){
-      c[N*j+i] += a[j*N+k] * b[N*k+i];
+    for (int p = 0; p<K; p++){
+      c[N*i+j] += a[i*K+p] * b[N*p+j];
     }
 }
 
 // Código host
 int main(){
-  int a[M*N], b[M*N], c[M*N];
+  int a[M*K], b[K*N], c[M*N];
   int* dev_a;
   int* dev_b;
   int* dev_c;
 
   // Inicializando as variaveis do host
-  for (int i = 0; i < N*M; i++){
+  for (int i = 0; i < M*K; i++){
     a[i] = i;
+  for (int i = 0; i < K*N; i++)
     b[i] = i*2;
   }
 
   // Alocando espaço para as variaveis da GPU
-  int tam = N*M*sizeof(int);
-  cudaMalloc((void**)&dev_a, tam);
-  cudaMalloc((void**)&dev_b, tam);
-  cudaMalloc((void**)&dev_c, tam);
+  cudaMalloc((void**)&dev_a, M*K*sizeof(int));
+  cudaMalloc((void**)&dev_b, K*N*sizeof(int));
+  cudaMalloc((void**)&dev_c, M*N*sizeof(int));
 
   // Copiando as variaveis da CPU para a GPU
-  cudaMemcpy(dev_a, &a, tam, cudaMemcpyHostToDevice);
-  cudaMemcpy(dev_b, &b, tam, cudaMemcpyHostToDevice);
+  cudaMemcpy(dev_a, &a, M*K*sizeof(int), cudaMemcpyHostToDevice);
+  cudaMemcpy(dev_b, &b, K*N*sizeof(int), cudaMemcpyHostToDevice);
 
   // Chamada à função da GPU (kernel)
   // Número de blocos é igual à dimensão do vetor dividida pela dimensão do bloco: N/M
@@ -49,8 +50,23 @@ int main(){
   multiplica_matriz<<<numBlocos, numThreads>>>(dev_a, dev_b, dev_c);
 
   // Copiando o resultado da GPU para CPU
-  cudaMemcpy(&c, dev_c, tam, cudaMemcpyDeviceToHost);
+  cudaMemcpy(&c, dev_c, M*N*sizeof(int), cudaMemcpyDeviceToHost);
 
+  for (int i = 0; i < M; i++){
+    for (int j = 0; j < K; j++)
+      printf("%d ", a[K*i+j]);
+    printf("\n");
+  }
+ 
+ print("X");
+ 
+ for (int i = 0; i < K; i++){
+    for (int j = 0; j < N; j++)
+      printf("%d ", b[N*i+j]);
+    printf("\n");
+  }
+ 
+ 
   // Visualizando o resultado
   for (int i = 0; i < M; i++){
     for (int j = 0; j < N; j++)
